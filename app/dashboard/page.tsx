f9f8721bc589f95f8823b1fd7b1f6d264bc467ff
NabiaHaider <nabiahaider11@gmail.com>
2025-01-13 21:45:47 +0500

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold mb-5'>Dashboard</h1>
+    <p className='mb-5'>Welcome to the Dashboard</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home (){
+  return (
+    <div>
+      <h1 className="text-2xl font-bol mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo site for the Traversy Media Next.js & Clerk 
+        tutorial. Go 
+        ahead and sign up or sig in!
+      </p>
+    </div>
+  )
+}
